ab956d0a3c68dbcfb54b24391a295d11a411917e
Ashwin <lateashwin@gmail.com>
2022-07-17 05:38:07 +0530

    Init

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
